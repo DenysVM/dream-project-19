48488959285f3f2e63889cc971adc5b0b970ac79
DenysVM <muratov.d@gmail.com>
2023-05-09 09:52:46 +0200

    main-optimization

--- src/main.js
+++ src/main.js
@@ -41,3 +41,18 @@
     refs.modal.classList.toggle('is-hidden');
   }
 })();
+
+
+      const swiper = new Swiper('.mySwyper', {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        loop: true,
+        Keyboard: {
+          enabled: true,
+        },
+        navigation: {
+          nextEl: '.btn-next',
+          prevEl: '.btn-prev',
+        },
+      });
+    
