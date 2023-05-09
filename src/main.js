90f90b420b6028b2107590a3e3731e823fc47581
DenysVM <muratov.d@gmail.com>
2023-05-09 10:32:54 +0200

    main.js

--- src/main.js
+++ src/main.js
@@ -75,17 +75,4 @@
       this.el.parentNode.querySelector(".number-of-slides").innerHTML = numIndex.toString() + slIndex.toString();
     }
   }
-});
-    
-
-// встановлюємо контекст імпортування для папки js та її піддиректорій
-const requireModule = require.context('./js', true, /\.js$/)
-
-// імпортуємо всі файли з папки та її піддиректорій
-requireModule.keys().forEach(fileName => {
-// імпортуємо файл
-const importedModule = requireModule(fileName)
-// додаємо його до поточного модуля
-importedModule.default()
-})
-
+});   
